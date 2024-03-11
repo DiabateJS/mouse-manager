bd0082807715a6794df07f41c3ff3965036c2e52
DiabateJS <diabatejs@yahoo.fr>
2024-03-11 23:16:45 +0100

    Début implémentation interface parametrage.

--- mousegenerator.py
+++ mousegenerator.py
@@ -50,12 +50,12 @@ class Application(Tk):
 
     def createWidgets(self): 
         self.title("Utilitaire de déplacement de souris")
-        self.geometry("600x450") 
+        self.geometry("600x600") 
 
         self.notebook = ttk.Notebook(self)
-        self.notebook.pack(pady=10, expand=True)
+        self.notebook.pack(expand=True)
 
-        self.deplacements = ttk.Frame(self.notebook, width=600, height=400)
+        self.deplacements = ttk.Frame(self.notebook, width=600, height=600)
         self.label = Label(self.deplacements,text='Choix de la stratégie de déplacement', font=("Arial", 18))
         self.label.pack()
 
@@ -72,10 +72,34 @@ class Application(Tk):
         self.combostrategies.pack(pady=10)
 
         self.bouton = Button(self.deplacements, text="Lancer", width=15, height=3, font=("Arial",14), command=self.activer)
-        self.bouton.pack(pady=5)
+        self.bouton.pack(pady=10)
 
-        self.parametrages = ttk.Frame(self.notebook, width=600, height=200)
-        self.apropos = ttk.Frame(self.notebook, width=600, height=200)
+        self.parametrages = ttk.Frame(self.notebook, width=600, height=600)
+        self.list_trajectoire = Listbox(self.parametrages)
+        self.list_trajectoire.insert(1, 'Triangle')
+        self.list_trajectoire.insert(2, 'Carre')
+        self.list_trajectoire.insert(3, 'Z')
+
+        self.list_trajectoire.bind('<<ListboxSelect>>',self.trajectoire_select)
+        self.list_trajectoire.pack()
+
+        self.list_points_trajectoire = Listbox(self.parametrages)
+        self.list_points_trajectoire.bind('<<ListboxSelect>>', self.point_select)
+        self.list_points_trajectoire.pack(pady=5)
+
+        self.label_point_x = Label(self.parametrages, text="X = ")
+        self.label_point_x.pack()
+        self.point_x_var = StringVar()
+        self.point_x = Entry(self.parametrages, textvariable=self.point_x_var)
+        self.point_x.pack()
+
+        self.label_point_y = Label(self.parametrages, text="Y = ")
+        self.label_point_y.pack()
+        self.point_y_var = StringVar()
+        self.point_y = Entry(self.parametrages, textvariable=self.point_y_var)
+        self.point_y.pack()
+
+        self.apropos = ttk.Frame(self.notebook, width=600, height=600)
         self.label_textapropos = Label(self.apropos, text='Conception et Développement', font=("Arial",16))
         self.label_textapropos.pack(pady=30)
         self.label_entreprise = Label(self.apropos, text='DJS TECHNOLOGIES', font=("Arial",32,"italic"), fg="blue")
@@ -89,6 +113,7 @@ class Application(Tk):
 
         self.deplacements.pack(fill='both', expand=True)
         self.parametrages.pack(fill='both', expand=True)
+
         self.apropos.pack(fill='both', expand=True)
 
         self.notebook.add(self.deplacements, text='Déplacements')
@@ -101,9 +126,37 @@ class Application(Tk):
         self.mainloop()
 
     def activer(self):
-        trajectoireBuilder = TrajectoireBuilder(self.choix_combo.get())
-        trajectoire = trajectoireBuilder.build()
-        trajectoire.deplacerSurPoints()
+        print(self.choix_combo.get())
+        if self.choix_combo.get() != "Autre":
+            trajectoireBuilder = TrajectoireBuilder(self.choix_combo.get())
+            trajectoire = trajectoireBuilder.build()
+            trajectoire.deplacerSurPoints()
+        else:
+            messagebox.showwarning(message="Veuillez choisir une stratégie de déplacement")
+    
+    def trajectoire_select(self, event):
+        select_index = self.list_trajectoire.curselection()[0]
+        tab_trajectoires = ('Triangle','Z','Carre')
+        if select_index < len(tab_trajectoires):
+            trajectoire = tab_trajectoires[select_index]
+            trajectoirBuilder = TrajectoireBuilder(trajectoire)
+            t = trajectoirBuilder.build()
+            self.list_points_trajectoire.delete(0, END)
+            for index in range(len(t.positions)):
+                self.list_points_trajectoire.insert(index, t.positions[index])
+    
+    def point_select(self, event):
+        index = self.list_points_trajectoire.curselection()[0]
+        select_item = self.list_points_trajectoire.get(index)
+        print(select_item)
+        xy_str = select_item.split("(")[1].split(")")[0] 
+        print(xy_str)
+        xy_tab = xy_str.split(",")
+        x = xy_tab[0]
+        y = xy_tab[1]
+        self.point_x_var.set(x)
+        self.point_y_var.set(y)
 
+            
 if __name__ == "__main__":
     app = Application()
